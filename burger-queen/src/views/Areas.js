23e23e16537fdab75ee7ad2115610db329277e93
TaniaTenorio <taniatenorio@hotmail.com>
2019-09-19 17:25:43 -0500

    se crea la carpeta views para añadir las vistas

---
+++ burger-queen/src/views/Areas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../components/Button';
+
+class Areas extends React.Component {
+    render() {
+        return <div>
+            <Button text = "Servicio" />
+            <Button text = "Cocina" />
+        </div>
+    }
+}
+
+export default Areas
