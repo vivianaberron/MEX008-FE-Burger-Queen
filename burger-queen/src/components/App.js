d178eb7c97f976103cb58608636ac6de9aed484c
Viviana <vivianaberron@gmail.com>
2019-09-20 18:18:14 -0500

    Añadiendo botón de next con imagen y modal de promoción del día

--- burger-queen/src/components/App.js
+++ burger-queen/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 import Login from './login';//para logearse con firebase
-//import Layout from './Layout'; // para el siseño de nuestra pagina
-//import Button from './Button';
+
+import Layout from '../views/Layout'; // para el siseño de nuestra pagina
+import Areas from '../views/Areas';
+
 //import Home from '../Home'; //para la pagina principal
 
 
@@ -11,17 +13,21 @@ function App() {
   return (
     
     <BrowserRouter>
-     
-        <Switch>
-          <Route exact path="/" component={Login} />
-        </Switch>
-      
-    </BrowserRouter> 
+
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Layout>
+          <Route exact path="/Areas" component={Areas} />
+        </Layout>
+      </Switch>
+    </BrowserRouter>
     // <div>
     //   <Button text = 'Servicio'/>
-    //   <Button text = 'Cocina'/>
-    // </div>
-    
+    // <Button text = 'Cocina'/>
+    //</div>
+
   );
 }
 export default App;
+
+
--- burger-queen/src/index.js
+++ burger-queen/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './components/styles/index.css';//importamos bootstrap
-import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap.css';
 import App from './components/App';
 
 
