0ab1e3aa51504f74d388d8fd2af45cb91365a459
TaniaTenorio <taniatenorio@hotmail.com>
2019-09-20 14:09:46 -0500

    se edita el boton de Iniciar para darle funcionalidad

--- burger-queen/src/components/login.js
+++ burger-queen/src/components/login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles/login.css';
 import logodemonium from '../img/logodemonium.png';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'; // componente de botones de red social
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {  Link  } from "react-router-dom";
 
 /*import withFirebaseAuth from 'react-with-firebase-auth';
 import * as firebase from 'firebase/app';
@@ -27,7 +27,9 @@ class Login extends React.Component {
           <Input type="number" placeholder="Ingresa tu número de empleado"></Input>
 
         </FormGroup>
-        <Link to="/Areas" className="btn-lg btn-warning btn-block">Iniciar</Link>
+        <Button color="warning">
+          <Link to="/Areas">Iniciar</Link>
+        </Button>
       </Form>
    );
  }
