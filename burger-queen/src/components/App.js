d901022e1fed6849d95fa9b853cae017f895b25e
TaniaTenorio <taniatenorio@hotmail.com>
2019-09-25 15:27:47 -0500

    se eliminan los JSON duplicados

--- burger-queen/src/components/App.js
+++ burger-queen/src/components/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
 import Login from './login';//para logearse con firebase
 import Layout from '../views/Layout'; // para el siseño de nuestra pagina
 import Areas from '../views/Areas';
 import Tips from '../views/Tips';
-import FlowStepper from './Stepper';
-
 import Home from '../views/Home';
 import Menu from '../views/Menu';
+import Registro from '../views/Registro';
+import EnvioOrden from '../views/EnvioOrden';
 
 
 
 function App() {
   return (
-    
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Login} />
@@ -21,12 +22,12 @@ function App() {
           <Route path="/Areas" component={Areas} />
           <Route path="/Tips" component={Tips} />
           <Route path="/Home" component={Home} />
-          <Route path="/Stepper" component={FlowStepper} />
           <Route path="/Menu" component={Menu} />
+          <Route path="/Registro" component={Registro} />
+          <Route path="/EnvioOrden" component={EnvioOrden} />
         </Layout>
       </Switch>
     </BrowserRouter>
-
   );
 }
 export default App;
--- burger-queen/src/components/Navbar.js
+++ burger-queen/src/components/Navbar.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import '../styles/Navbar.css';
-
-import '../styles/index.css'
 import logo from '../img/logo.png';
 import logodemonium from '../img/logodemonium.png';
 
