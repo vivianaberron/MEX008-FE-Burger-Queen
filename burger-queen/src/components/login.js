78014432ef037cf06bd4c7dfdd1950b494b4e63d
Pamela C. Torres Gutiérrez <pamelatgtz@gmail.com>
2019-09-29 21:38:40 -0500

    Trabajando con conexion de bd en firebase para el logeo

--- burger-queen/src/components/login.js
+++ burger-queen/src/components/login.js
@@ -4,15 +4,27 @@ import logodemonium from '../img/logodemonium.png';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'; // componente de botones de red social
 import {  Link  } from "react-router-dom";
 
-
-
-//comienza diseño con bootstrap
-
+import * as ROUTES from '../../constants/routes';
 
 class Login extends React.Component {
+//comienzo con la conexion con firebase
+handChange= e =>{
+  console.log({
+    no_empleado:e.target.no_empleado,
+  });
+};
+
+handleClick = e => {
+  console.log('Button was clicked');
+};
+handleSubmit = e => {
+  e.preventDefault ();
+  console.log('Form was submitted');
+};
+
   render() {
   return ( 
-      <Form className="App">
+      <Form onSubmit={this.handleSubmit} className="App">
         <div><img className="login_header" src={logodemonium} alt="Logo de la compañia"/></div>
         <h1 className="text-center">Inicia sesión</h1>
         <h2>
